5aeb16327ada75bcc3fbca06eefaae87b025af42
platonso <plat100916@gmail.com>
2024-09-18 02:26:10 +0300

    add manual operation

---
+++ app/src/main/java/com/platon/kotlinmirealabs/FirstFragment.kt
@@ -0,0 +1,26 @@
+package com.platon.kotlinmirealabs
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.braintreepayments.cardform.view.CardForm
+import com.platon.kotlinmirealabs.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+
+        return binding.root
+    }
+
+}
--- app/src/main/java/com/platon/kotlinmirealabs/MainActivity.kt
+++ app/src/main/java/com/platon/kotlinmirealabs/MainActivity.kt
@@ -5,16 +5,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.platon.kotlinmirealabs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
+
+        binding.button1.setOnClickListener {
+            replaceFragment(FirstFragment())
+        }
+        binding.button2.setOnClickListener {
+            replaceFragment(SecondFragment())
+        }
+        binding.button3.setOnClickListener {
+            replaceFragment(ThirdFragment())
+        }
+    }
+
+    private fun replaceFragment(newFragment: Fragment) { // Ручное управление транзакцией
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment, newFragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
