f393a83eb3c97de08da43f3d805a80e10f1c5ccf
platonso <plat100916@gmail.com>
2024-09-26 13:24:53 +0300

    add Text

--- app/src/main/java/com/platon/kotlinmirealabs/MainActivity.kt
+++ app/src/main/java/com/platon/kotlinmirealabs/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.platon.kotlinmirealabs.ui.theme.KotlinMireaLabsTheme
+import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +27,7 @@ class MainActivity : ComponentActivity() {
             KotlinMireaLabsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Солозобов Платон",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,16 +38,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KotlinMireaLabsTheme {
-        Greeting("Android")
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "$name ИКБО-11-22",
+            style = MaterialTheme.typography.bodyLarge,
+            color = Color.Cyan
+        )
     }
 }
